b426cb744d546d340fcb5f1b26ff736a35dcf0e2
BitNers <wesleyalvessilva@outlook.com.br>
2023-01-20 22:21:16 -0300

    Adding First Impressions Project

---
+++ FirstImpressions/AsyncTasks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstImpressions
+{
+    internal class AsyncTasks
+    {
+
+        async void TarefaDemorada() {
+
+            var tarefa1 = Task.Run(() =>
+            {
+                Console.WriteLine("[Async] Rodando por 15 segundos...");
+                Thread.Sleep(15000);
+                Console.WriteLine("[Async] Terminei :)");
+
+                return;
+            });
+
+            await tarefa1;
+            
+        }
+
+        public string PontoDeRequest(string palavra) {
+            TarefaDemorada();
+            return "[Sync] Retornei " + palavra;
+        }
+    }
+}
---
+++ FirstImpressions/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstImpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            AsyncTasks testAsync = new AsyncTasks();
+
+            string palavra = testAsync.PontoDeRequest("Assincrono");
+            Console.WriteLine(palavra);
+
+            Thread.Sleep(30000);
+        }
+    }
+}

9294e5d13b1673d142912a1f414409b5fec4c9a0
BitNers <wesleyalvessilva@outlook.com.br>
2023-01-23 08:49:22 -0300

    Adding LINQ Models

---
+++ LINQ/EntityFramework/Escola/AlunoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.EntityFramework.Escola
+{
+    [Table("Aluno")]
+    internal class AlunoModel
+    {
+        [Key]
+        public int AlunoId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? Nome { get; set; }
+
+        public List<MateriaModel>? Materias { get; set; }
+
+    }
+}
---
+++ LINQ/EntityFramework/RoleModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LINQ.EntityFramework
+{
+    [Table("Roles")]
+    internal class RoleModel
+    {
+        [Key]
+        public int RoleId { get; set; }
+        [Required]
+        public string? Code { get; set; }
+        [Required]
+        public string? Description { get; set; }
+    }
+}

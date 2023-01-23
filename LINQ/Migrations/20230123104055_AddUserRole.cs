568e0b1b2ceb5c3cbc11729a948e2bf9c8d2c2f7
BitNers <wesleyalvessilva@outlook.com.br>
2023-01-23 08:49:45 -0300

    Adding Database Migrations

---
+++ LINQ/Migrations/20230123104055_AddUserRole.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LINQ.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_RoleModel_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RoleModel",
+                table: "RoleModel");
+
+            migrationBuilder.RenameTable(
+                name: "RoleModel",
+                newName: "Roles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Roles_RoleId",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Roles",
+                table: "Roles");
+
+            migrationBuilder.RenameTable(
+                name: "Roles",
+                newName: "RoleModel");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RoleModel",
+                table: "RoleModel",
+                column: "RoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_RoleModel_RoleId",
+                table: "Users",
+                column: "RoleId",
+                principalTable: "RoleModel",
+                principalColumn: "RoleId");
+        }
+    }
+}
---
+++ LINQ/Migrations/20230123105024_CreatingEscolaMigration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LINQ.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingEscolaMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aluno",
+                columns: table => new
+                {
+                    AlunoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aluno", x => x.AlunoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Materias",
+                columns: table => new
+                {
+                    MateriaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materias", x => x.MateriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AlunoModelMateriaModel",
+                columns: table => new
+                {
+                    AlunosAlunoId = table.Column<int>(type: "int", nullable: false),
+                    MateriasMateriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlunoModelMateriaModel", x => new { x.AlunosAlunoId, x.MateriasMateriaId });
+                    table.ForeignKey(
+                        name: "FK_AlunoModelMateriaModel_Aluno_AlunosAlunoId",
+                        column: x => x.AlunosAlunoId,
+                        principalTable: "Aluno",
+                        principalColumn: "AlunoId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlunoModelMateriaModel_Materias_MateriasMateriaId",
+                        column: x => x.MateriasMateriaId,
+                        principalTable: "Materias",
+                        principalColumn: "MateriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlunoModelMateriaModel_MateriasMateriaId",
+                table: "AlunoModelMateriaModel",
+                column: "MateriasMateriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlunoModelMateriaModel");
+
+            migrationBuilder.DropTable(
+                name: "Aluno");
+
+            migrationBuilder.DropTable(
+                name: "Materias");
+        }
+    }
+}

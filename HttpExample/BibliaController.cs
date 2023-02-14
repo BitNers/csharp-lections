b2bc6180db51b32f597b3ad3dd881cbd07165268
BitNers <wesleyalvessilva@outlook.com.br>
2023-02-14 11:41:38 -0300

    Create BibliaController

---
+++ HttpExample/BibliaController.cs
@@ -0,0 +1,51 @@
+﻿using HttpExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace HttpExample
+{
+    class BibliaController
+    {
+        private HttpClient _httpCli;
+        private readonly string _uri;
+        private readonly HttpMethod _method;
+
+        public BibliaController(string Uri, HttpMethod method)
+        {
+            _uri = Uri;
+            _method = method;
+            _httpCli = new HttpClient();
+        }
+ 
+
+        public async Task<VersiculoModel> GetVersiculos() {
+            
+            using (var requisicaoHTTP = new HttpRequestMessage(_method, _uri))
+            {
+                VersiculoModel result = new VersiculoModel();
+
+                requisicaoHTTP.Version = HttpVersion.Version11;
+
+                string responseBody = "";
+                using HttpResponseMessage httpResponse = _httpCli.Send(requisicaoHTTP);
+                if(httpResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    responseBody = await httpResponse.Content.ReadAsStringAsync() ?? string.Empty;
+                    result = JsonConvert.DeserializeObject<VersiculoModel>(responseBody);
+                    result.reference = result.error;
+                    return result;
+                }
+
+                responseBody = await httpResponse.Content.ReadAsStringAsync() ?? string.Empty;
+                result = JsonConvert.DeserializeObject<VersiculoModel>(responseBody);
+                return result;
+            }
+        }
+    }
+}

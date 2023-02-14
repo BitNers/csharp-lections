55d4ec69da46dc7f1cdc4cc5f58c5e89385ede59
BitNers <wesleyalvessilva@outlook.com.br>
2023-02-14 11:42:22 -0300

    Update Http Core Program
    Will run a ReadLine Console, manipulating requests for Bible API

--- HttpExample/Program.cs
+++ HttpExample/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using HttpExample;
+using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Mime;
 
 /*
@@ -6,11 +8,60 @@ using System.Net.Mime;
     Site com mútliplos Endpoints para testar e praticar HTTP em código.
  */
 
+while (true) {
+
+    
+    string versiculo = Console.ReadLine().Trim() ?? "";
+    if(versiculo == string.Empty) continue;
+
+    if (versiculo.ToLower() == "exit")
+        Environment.Exit(0);
+
+    string uri = @$"https://bible-api.com/{versiculo}?translation=almeida";
+    Console.WriteLine(uri);
+    BibliaController bibleController = new BibliaController(uri, HttpMethod.Get);
+
+    var versiculos = bibleController.GetVersiculos();
+    versiculos.Wait();
+
+    Console.WriteLine(versiculos.Result.ToString());
+
+
+}
+
+
+
+
+HttpClient httpSync = new HttpClient();
+using (var requestHTTPSync = new HttpRequestMessage(HttpMethod.Get, "https://httpbin.org/get"))
+{
+    requestHTTPSync.Headers.Authorization = new AuthenticationHeaderValue("bearer", "bearertoken");
+    requestHTTPSync.Content = new StringContent("Fluminense e Vasco no Domingo.");
+
+    var responseHTTP = httpSync.Send(requestHTTPSync);
+    
+    responseHTTP.EnsureSuccessStatusCode();
+
+    if (responseHTTP.StatusCode != HttpStatusCode.OK)
+        return;
+
+    Console.WriteLine(responseHTTP.ToString());
+    Console.WriteLine(responseHTTP.Content);
+}
+
+
+System.Environment.Exit(0);
 
 async Task MakeRequest(string Uri, HttpMethod method, string body = "") {
 
     try
     {
+        // Mais liberdade em usar Requisições (HTTP, FTP, SMTP, et cetera)
+         // WebRequest request = new WebRequest();
+        
+        // Tenho mais liberdade em criar Requisições HTTP
+        // HttpWebRequest webReq = new HttpWebRequest(Uri);
+            
         HttpClient httpCli = new HttpClient();
         string requestUriGet = Uri;
 
@@ -19,10 +70,12 @@ async Task MakeRequest(string Uri, HttpMethod method, string body = "") {
             if(body != string.Empty)
                 requestHTTP.Content = new StringContent(body);
 
+            // requestHTTP.Properties.Add("Content-Type", "application/json");
+            requestHTTP.Version = HttpVersion.Version11;
             requestHTTP.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "tokenBom");
             
 
-            using HttpResponseMessage httpResponse = await httpCli.SendAsync(requestHTTP);
+            using HttpResponseMessage httpResponse = httpCli.Send(requestHTTP);
             httpResponse.EnsureSuccessStatusCode();
 
             /*

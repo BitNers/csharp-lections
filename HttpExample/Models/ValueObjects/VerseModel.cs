39807a2ea724f5717502e0208a6f2cb6d38962b9
BitNers <wesleyalvessilva@outlook.com.br>
2023-02-14 11:41:51 -0300

    Creating Versicules Class Struct

---
+++ HttpExample/Models/ValueObjects/VerseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpExample.Models.ValueObjects
+{
+    class VerseModel
+    {
+        public string book_id{ get; set; }
+        public string book_name { get; set; }
+        public int chapter { get; set; }
+        public int verse { get; set; }
+        public string text { get; set; }
+    }
+}
---
+++ HttpExample/Models/VersiculoModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpExample.Models.ValueObjects;
+
+namespace HttpExample.Models
+{
+    class VersiculoModel
+    {
+        public string reference { get; set; }
+        public ICollection<VerseModel> verses { get; set; }
+        public string text { get; set; }
+        public string translation_id { get; set; }
+        public string translation_name { get; set; }
+        public string translation_note { get; set; }
+
+        public string error { get; set; }
+
+
+        
+        public override string ToString() {
+            string rt = string.Empty;
+            foreach (var verse in verses) {
+                rt += $"\t [{verse.book_name} {verse.chapter}:{verse.verse}]: '{verse.text.Trim()}'\n".Trim();
+                rt += "\n";
+            }
+            return rt.Trim();
+        }
+
+    }
+}

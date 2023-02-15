bdd484d849e6cd0f70ba99c065fd2d0389c6b7a8
BitNers <wesleyalvessilva@outlook.com.br>
2023-02-14 22:04:52 -0300

    Create HttpBinRepo

---
+++ HttpAulaPratica/Repositorio/HttpBin/HttpBinRepo.cs
@@ -0,0 +1,28 @@
+﻿using HttpAulaPratica.HttpFactory;
+using HttpAulaPratica.Models.Bible;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAulaPratica.Repositorio.HttpBin
+{
+    class HttpBinRepo : IHttpBinRepo
+    {
+        private readonly HttpFabrica _httpFabrica;
+        public HttpBinRepo(string uri, HttpMethod method)
+        {
+            _httpFabrica = new HttpFabrica(uri, method);
+        }
+
+        public string SendData(BibleReferenceModel data)
+        {
+            var httpResponse = _httpFabrica.CreateRequestPostAsync("", data);
+            httpResponse.Wait();
+
+
+            return httpResponse.Result ?? "";
+        }
+    }
+}
---
+++ HttpAulaPratica/Repositorio/HttpBin/IHttpBinRepo.cs
@@ -0,0 +1,14 @@
+﻿using HttpAulaPratica.Models.Bible;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpAulaPratica.Repositorio.HttpBin
+{
+    interface IHttpBinRepo
+    {
+        string SendData(BibleReferenceModel data);
+    }
+}

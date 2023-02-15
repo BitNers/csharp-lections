65e47f5c8951cf3415a9fc1a0be8587230caba95
BitNers <wesleyalvessilva@outlook.com.br>
2023-02-14 22:06:22 -0300

    Http Aula Pratica

---
+++ HttpAulaPratica/HttpFabrica/HttpFabrica.cs
@@ -0,0 +1,85 @@
+﻿using HttpAulaPratica.Models.Bible;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace HttpAulaPratica.HttpFactory
+{
+    class HttpFabrica
+    {
+        private readonly HttpClient _httpClient;
+        private string _uri;
+        private readonly HttpMethod _method;
+        public HttpFabrica(string uri, HttpMethod method)
+        {
+            _uri = uri;
+            _method = method;
+            _httpClient= new HttpClient();
+        }
+
+        public async Task<string> CreateRequestPostAsync(string parameters = "", BibleReferenceModel data = null) {
+
+            if (parameters != string.Empty)
+            {
+                _uri += parameters;
+            }
+
+            using (HttpRequestMessage httpRequest = new HttpRequestMessage(_method, _uri))
+            {
+
+                httpRequest.Method = _method;
+                httpRequest.Headers.Add("Accept", "application/json");
+                httpRequest.Version = HttpVersion.Version11;
+                string ObjToTxt = "{}";
+                if (data != null)
+                    ObjToTxt = JsonSerializer.Serialize(data);
+
+                if(ObjToTxt != null) { httpRequest.Content = new StringContent(ObjToTxt); }
+                
+
+                HttpResponseMessage result = _httpClient.Send(httpRequest);
+                if (!result.IsSuccessStatusCode) { return "Not found"; }
+                if (result.Content != null)
+                {
+                    string ResultDataString = await result.Content.ReadAsStringAsync();
+                    return ResultDataString;
+
+                }
+                return string.Empty;
+
+            }
+        }
+
+        public async Task<BibleReferenceModel?> CreateRequestAsync(string parameters = "")
+        {
+
+            if(parameters != string.Empty)
+            {
+                _uri += parameters;   
+            }
+            
+
+            using (HttpRequestMessage httpRequest = new HttpRequestMessage(_method, _uri))
+            {
+                
+                httpRequest.Method = _method;
+                httpRequest.Version = HttpVersion.Version11;
+
+                HttpResponseMessage result = _httpClient.Send(httpRequest);
+                if (!result.IsSuccessStatusCode) { BibleReferenceModel bible = new BibleReferenceModel{ error = "not found"}; }
+                if (result.Content != null)
+                {
+                    string ResultDataString = await result.Content.ReadAsStringAsync();
+                    BibleReferenceModel bible = JsonSerializer.Deserialize<BibleReferenceModel>(ResultDataString);
+                    return bible;
+                    
+                    
+                }
+                return null;
+
+            }
+
+        }
+
+    }
+}
---
+++ HttpAulaPratica/Program.cs
@@ -0,0 +1,31 @@
+﻿using HttpAulaPratica.Repositorio.Bible;
+using HttpAulaPratica.Repositorio.HttpBin;
+
+while (true) {
+
+    string capitulo = Console.ReadLine().Trim() ?? "";
+    if (capitulo == string.Empty) { continue; }
+
+    /*
+        1. Aplicação Recebe Parametros para Ler os Versiculos da Biblia (https://bible-api.com/BOOK+CHAPTER:VERSE)
+        2. Nosso BibleRepo irá chamar a Fábrica de HTTP para puxar os dados do capítulo requisitado.
+        3. Depois, nosso HttpBinRepo irá Serializar o Objeto retornado da API da Biblia
+        4. Enviará um POST para ver os dados ecoados na resposta HTTP.
+     */
+
+    var bibleControllerGet = new BibleRepo("https://bible-api.com/",HttpMethod.Get);
+    var bibleResult = bibleControllerGet.GetVerses(capitulo);
+
+    bibleResult.text += "**";
+
+    Console.WriteLine(bibleResult.ToString());
+    // var dataToSend = bibleResult.ToString();
+
+    var httpBinControllerPost = new HttpBinRepo("https://httpbin.org/post", HttpMethod.Post);
+
+    var HttpBinResponse = httpBinControllerPost.SendData(bibleResult);
+    
+    
+    Console.WriteLine(HttpBinResponse);
+    
+}
